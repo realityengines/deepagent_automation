65593ed1b1bb2e13e946db377a091dd0bd184273
abacusUday <udaysingh@abacus.ai>
2025-06-27 20:44:00 +0530

    name change

--- features/support/world.js
+++ features/support/world.js
@@ -14,7 +14,7 @@ class BrowserType extends World {
           "LT:Options": {
             platform: config.lambdaTest.platformName,
             build: config.lambdaTest.buildName,
-            name: "Playwright Test",
+            name: "Deeo Agent Test",
             user: config.lambdaTest.username,
             accessKey: config.lambdaTest.accessKey,
             network: false,
